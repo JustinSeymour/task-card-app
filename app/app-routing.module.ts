51b8c44fff70c96e4c2b00b52ca6e97ceb146256
Justin Seymour <justin@3wmholdings.com>
2018-03-23 07:02:51 +0200

    Implemented routing

---
+++ app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+import { Router } from '@angular/router/src/router';
+
+@NgModule({
+    imports: [ 
+        RouterModule.forRoot([
+  
+        ])
+     ],
+     exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BugModule } from './bugs/bug.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({ 
-    imports: [ BrowserModule ],
+    imports: [ BrowserModule,
+               BugModule,
+               AppRoutingModule 
+             ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 })
