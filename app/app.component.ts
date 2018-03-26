c246da33a736ba2ec3eb1ba12013e4713885fd17
Justin Seymour <justin@3wmholdings.com>
2018-03-26 05:58:52 +0200

    Lesson font awesome

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component ({
     selector: 'my-app',
-    template: '<router-outlet></router-outlet>'
+    template: `
+    <navbar></navbar>
+    <div class="container">
+    <router-outlet></router-outlet>
+    </div>
+    `,
+    styles: [`
+        .container {
+            margin-top: 5rem;
+        }
+    `]
 })
 
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,17 @@ import { BugModule } from './bugs/bug.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({ 
     imports: [ BrowserModule,
                BugModule,
-               AppRoutingModule 
+               AppRoutingModule
              ],
-    declarations: [ AppComponent ],
+    declarations: [ 
+        AppComponent,
+        NavbarComponent 
+    ],
     bootstrap: [ AppComponent ]
 })
 
